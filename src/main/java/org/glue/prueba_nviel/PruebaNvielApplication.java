9e04c5af4e0a8a19a804aefd12f3c18067503637
andre <a@a.es>
2025-06-11 18:51:54 +0200

    Initial commit

---
+++ src/main/java/org/glue/prueba_nviel/PruebaNvielApplication.java
@@ -0,0 +1,13 @@
+package org.glue.prueba_nviel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaNvielApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaNvielApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/glue/prueba_nviel/PruebaNvielApplicationTests.java
@@ -0,0 +1,13 @@
+package org.glue.prueba_nviel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaNvielApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
