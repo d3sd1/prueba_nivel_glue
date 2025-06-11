38eaede6593b2fc6f7173226c0257b80d55d8297
andre <a@a.es>
2025-06-11 21:26:10 +0200

    habilitado h2 pruebas iniciales

--- src/main/java/org/glue/prueba_nivel/PruebaNivelApplication.java
+++ src/main/java/org/glue/prueba_nivel/PruebaNivelApplication.java
@@ -1,9 +1,12 @@
 package org.glue.prueba_nivel;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Product Sorting API", version = "1.0", description = "API for sorting and scoring products"))
 public class PruebaNivelApplication {
     public static void main(String[] args) {
         SpringApplication.run(PruebaNivelApplication.class, args);
--- src/main/java/org/glue/prueba_nivel/controller/dto/response/ProductResponseDTO.java
+++ src/main/java/org/glue/prueba_nivel/controller/dto/response/ProductResponseDTO.java
@@ -1,9 +1,15 @@
 package org.glue.prueba_nivel.controller.dto.response;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import org.glue.prueba_nivel.database.entity.StockEntity;
+
+import java.util.List;
 
 @Data
 public class ProductResponseDTO {
-    private String size;
-    private int units;
+    private Integer id;
+    private String name;
+    private Integer salesUnits;
+    private List<StockResponseDTO> stocks;
 }
